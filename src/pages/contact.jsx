ebf4d00d0c916513b298d9d78b8c75759b9a0c4f
SirojiddinCoder <sirojiddincoder@gmail.com>
2024-06-03 22:02:32 -0700

    languages qismi toliq bitdi

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -5,8 +5,13 @@ import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './contact.css';
+import { useTranslation } from "react-i18next";
+
+
 
 export const Contact = () => {
+  const { t } = useTranslation();
+
   useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);
@@ -21,15 +26,15 @@ export const Contact = () => {
       <div className="container">
         <div className="contact-us">
           <div className="contact-us-left" data-aos="fade-right">
-            <h2 className='contact-title'>Aloqa</h2>
+            <h2 className='contact-title'>{t('contact.Aloqa')}</h2>
             <form action="#">
-              <input className='contact-input' type="text" placeholder='Ismingiz' data-aos="fade-up" /> <br />
-              <input className='contact-input' type="tel" placeholder='tel raqamingiz' data-aos="fade-up" data-aos-delay="200" /> <br />
-              <textarea className='contact-textAre' name="message" id="message" placeholder='bizga habar yuboring' data-aos="fade-up" data-aos-delay="400"></textarea>
-               <div className="submit" data-aos="fade-up" data-aos-delay="600">
-                <a className='send-btn' href="#">Yuborish</a>
-               </div>
-            </form>
+    <input className='contact-input' type="text" placeholder={t('contact.Ismingiz')} data-aos="fade-up" /> <br />
+    <input className='contact-input' type="tel" placeholder={t('contact.telraqamingiz')} data-aos="fade-up" data-aos-delay="200" /> <br />
+    <textarea className='contact-textAre' name="message" id="message" placeholder={t('contact.habar')} data-aos="fade-up" data-aos-delay="400"></textarea>
+    <div className="submit" data-aos="fade-up" data-aos-delay="600">
+        <a className='send-btn' href="#">{t('contact.Yuborish')}</a>
+    </div>
+</form>
           </div>
           <div className="contact-us-right" data-aos="fade-left">
             <YMaps query={{ apikey: 'YOUR_YANDEX_API_KEY' }}>
